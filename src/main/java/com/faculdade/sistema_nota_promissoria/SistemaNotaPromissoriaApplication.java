77b12d6622cc7d08310d7c53c21f25bc3d92a14c
Pedro Henrique <ph.laurindo2020@gmail.com>
2025-01-19 23:56:39 -0300

    first commit

---
+++ src/main/java/com/faculdade/sistema_nota_promissoria/SistemaNotaPromissoriaApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.sistema_nota_promissoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaNotaPromissoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaNotaPromissoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faculdade/sistema_nota_promissoria/SistemaNotaPromissoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculdade.sistema_nota_promissoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNotaPromissoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
